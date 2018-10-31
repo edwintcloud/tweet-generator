e45a4212134ed46616fee07cf1cd1fb8b7568588
Edwin Cloud <ecloud412@gmail.com>
2018-10-30 23:31:49 -0700

    :muscle: Weighted_generaator finished but still slow :scream:

--- weighted_generator.py
+++ weighted_generator.py
@@ -3,35 +3,31 @@ import word_frequency as wf
 
 
 def main(file_name, num_of_words):
+    '''Main entry point for program'''
 
     # get histogram from file using our word_frequency import
-    hg = wf.main(file_name)
+    histogram = wf.main(file_name)
 
-    # get our total number of words in source
-    total_words = sum(i[1] for i in hg)
+    # create list of words from histogram
+    words = [i[0] for i in histogram]
 
-    # create empty list to hold our random words
-    random_words = []
+    # create list of weights from histogram
+    weights = [i[1]/wf.total_words(histogram) for i in histogram]
 
-    # do random weighted generation until we have num_of_words words
-    while len(random_words) < int(num_of_words):
+    # generate random words based on weights
+    random_words = random.choices(words, weights, k=int(num_of_words))
 
-        # pick a random index
-        random_index = random.randrange(len(hg))
-
-        # generate probability percentage based on word frequency
-        word_probability = hg[random_index][1] / total_words
-
-        # generate random probability percentage based on total words in source
-        random_probability = random.random() * total_words
-
-        # if the word probability is greater than the random probability,
-        # add the word to random words list
-        if word_probability > random_probability:
-            random_words.append(hg[random_index])
-
-    print(random_words)
+    # return random words
+    return random_words
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+
+    # get random words
+    random_words = main(sys.argv[1], sys.argv[2])
+
+    # Capitalize first word
+    random_words[0] = random_words[0].capitalize()
+
+    # print out a sentence of the random words
+    print(' '.join(random_words)+'.')

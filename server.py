75356a6dbec5614953646c136d5bb664c24f2fc8
Edwin Cloud <ecloud412@gmail.com>
2018-11-05 13:33:43 -0800

    :sparkles: Created nice design for web frontend

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 import weighted_generator
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-  random_sentence = weighted_generator.main('the_republic.txt', 12)
-  return random_sentence
+@app.route('/', methods=['GET'])
+def main_page():
+  random_sentence = weighted_generator.generate_sentence('the_republic.txt', 12)
+  return  render_template('index.html', sentence = random_sentence)
+
+@app.route('/new_sentence', methods=['GET'])
+def new_sentence():
+  random_sentence = weighted_generator.generate_sentence('the_republic.txt', 12)
+  return jsonify(random_sentence)

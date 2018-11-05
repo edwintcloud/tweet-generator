fd50bc785e2eab070c167d3ef5b00f8b62f1bd1d
Edwin Cloud <ecloud412@gmail.com>
2018-11-05 14:40:39 -0800

    Added image of plato to webpage

--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,5 @@
 function new_sentence() {
   axios.get('/new_sentence').then(res => {
-    document.querySelector('.sentence').innerHTML = res.data
+    document.querySelector('#message').innerHTML = res.data
   })
 }

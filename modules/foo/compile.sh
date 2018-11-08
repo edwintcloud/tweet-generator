6a73eebf7325e1d4cafd4c70af80c50082e5cda0
Edwin Cloud <ecloud412@gmail.com>
2018-11-07 22:19:48 -0800

    :rainbow: Dictogram and Listogram classes implemented

--- modules/foo/compile.sh
+++
@@ -1,2 +0,0 @@
-g++ -c -fPIC foo.cpp -o foo.o
-g++ -shared -Wl,-install_name,libfoo.so -o libfoo.so  foo.o
--- modules/foo/fooWrapper.py
+++
@@ -1,9 +0,0 @@
-from ctypes import cdll
-lib = cdll.LoadLibrary('/Users/edwincloud/Documents/Make School/projects/python/tweet-generator/modules/foo/libfoo.so')
-
-class Foo(object):
-    def __init__(self):
-        self.obj = lib.Foo_new()
-
-    def bar(self):
-        lib.Foo_bar(self.obj)

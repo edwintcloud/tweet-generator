dfdb2e8caba24970f9c4cdf3d17ef632a566187d
Edwin Cloud <ecloud412@gmail.com>
2018-11-15 12:06:48 -0800

    :wrench: Fix delete method and run tests

--- now_deployment/scripts.js
+++
@@ -1,8 +0,0 @@
-function new_sentence() {
-  axios.get('/app.py').then(res => {
-    document.querySelector('#message').innerHTML = res.data;
-    document.querySelector('.container').style.display = 'flex';
-  }).catch(error => {
-    console.log(error.message);
-  })
-}
